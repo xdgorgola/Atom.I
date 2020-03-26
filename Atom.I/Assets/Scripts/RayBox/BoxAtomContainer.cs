abb829899eb50956010d8d57a4ff1cf8cb9422e5
xdgorgola <15-11264@usb.ve>
2020-03-25 22:06:46 -0430

    Container prototype

---
+++ Atom.I/Assets/Scripts/RayBox/BoxAtomContainer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(RayBox))]
+public class BoxAtomContainer : MonoBehaviour
+{
+    private RayBox box = null;
+
+    public Vector2 min { get; private set; } = Vector2.zero;
+    public Vector2 max { get; private set; } = Vector2.zero;
+
+    private List<GameObject> atomsInside = new List<GameObject>();
+
+    private void Awake()
+    {
+        box = GetComponent<RayBox>();
+        box.onBoxShoot.AddListener(CaptureAtoms);
+    }
+
+    public void CaptureAtoms()
+    {
+        Debug.Log("!!!!!!!!!!!!!!1");
+        min = box.min;
+        max = box.max;
+
+        Vector2 cornerTL = (Vector2)transform.position + Vector2.up * box.max.y + Vector2.right * box.min.x;
+        Vector2 cornerBR = (Vector2)transform.position + Vector2.up * box.min.y + Vector2.right * box.max.x;
+
+        Debug.DrawLine(cornerTL, cornerBR, Color.black, 4);
+        Debug.Log(cornerTL);
+        Debug.Log(cornerBR);
+        Collider2D[] atomsCaptured = Physics2D.OverlapAreaAll(cornerTL, cornerBR);
+        foreach (Transform atoms in atomsCaptured.Select(a => a.transform).Where(b => b.childCount != 0))
+        {
+            Debug.Log(atoms.name);
+            atoms.gameObject.SetActive(false);
+        }
+    }
+
+}
--- Atom.I/Assets/Scripts/RayBox/RayBox.cs
+++ Atom.I/Assets/Scripts/RayBox/RayBox.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(MouseFollower))]
 public class RayBox : MonoBehaviour
@@ -24,13 +26,16 @@ public class RayBox : MonoBehaviour
     [SerializeField]
     private float halfSize = 4f;
 
+    [SerializeField]
+    private float boxTime = 4f;
+
     // NOTA: PODRIAMOS USAR UN BOUNDING BOX DE UNITY QUE YA VIENE CON
     // POINT IS INSIDE.
     /// <summary>
     /// Bounding Box de la caja minimos y maximos. 
     /// </summary>
-    private Vector2 min = Vector2.zero;
-    private Vector2 max = Vector2.zero;
+    public Vector2 min { get; private set; } = Vector2.zero;
+    public Vector2 max { get; private set; } = Vector2.zero;
 
     /// <summary>
     /// Transformada de hijo que contendra los colliders externos
@@ -42,6 +47,9 @@ public class RayBox : MonoBehaviour
     /// </summary>
     private List<Collider2D> walls = new List<Collider2D>();
 
+    public UnityEvent onBoxShoot = new UnityEvent();
+    public UnityEvent onBoxReActivate = new UnityEvent();
+
     private void Awake()
     {
         mf = GetComponent<MouseFollower>();
@@ -51,8 +59,8 @@ public class RayBox : MonoBehaviour
 
     private void Start()
     {
-        max = Vector2.one * halfSize + (Vector2)transform.position;
-        min = -Vector2.one * halfSize + (Vector2)transform.position;
+        max = Vector2.one * halfSize;
+        min = -Vector2.one * halfSize;
 
         colliderContainer.localScale = ((Vector3.up + Vector3.right) * halfSize + Vector3.forward) * 2;
 
@@ -73,17 +81,26 @@ public class RayBox : MonoBehaviour
     public void ScaleBox(float scaleAmount)
     {
         halfSize = Mathf.Clamp(halfSize + scaleAmount, minHalfSize, maxHalfSize);
-        max = Vector2.one * halfSize + (Vector2)transform.position;
-        min = -Vector2.one * halfSize + (Vector2)transform.position;
+        max = Vector2.one * halfSize;
+        min = -Vector2.one * halfSize;
 
         colliderContainer.localScale = ((Vector3.up + Vector3.right) * halfSize + Vector3.forward) * 2;
+        //foreach (Transform coll in walls.Select(wallColl => wallColl.transform))
+        //{
+        //    coll.localScale = Vector3.right * 0.52f + Vector3.up * (0.77f / colliderContainer.localScale.y) + Vector3.forward;
+        //}
+        // El scale de los hijos debe de ser compensado en Y para que no aumenten su grosor.
+        // dividirlo entre la escala del padre creo que seria suficiente.
+        // Colliders a 0.75 no estarian mal
     }
 
 
     public void ShootBox()
     {
+        ScaleBox(0);
         mf.SetMFStatus(false);
         SetWallsStatus(true);
+        onBoxShoot.Invoke();
     }
 
 
@@ -99,10 +116,11 @@ public class RayBox : MonoBehaviour
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine((Vector2)transform.position + Vector2.right * min.x + Vector2.up * max.y, (Vector2)transform.position + Vector2.right * max.x + Vector2.up * max.y);
-        Gizmos.DrawLine((Vector2)transform.position + Vector2.right * min.x + Vector2.up * max.y, (Vector2)transform.position + Vector2.right * min.x + Vector2.up * min.y);
-        Gizmos.DrawLine((Vector2)transform.position + Vector2.right * min.x + Vector2.up * min.y, (Vector2)transform.position + Vector2.right * max.x + Vector2.up * min.y);
-        Gizmos.DrawLine((Vector2)transform.position + Vector2.right * max.x + Vector2.up * min.y, (Vector2)transform.position + Vector2.right * max.x + Vector2.up * max.y);
+        Vector2 origin = (Vector2)transform.position;
+        Gizmos.DrawLine(origin + Vector2.right * min.x + Vector2.up * max.y, origin + Vector2.right * max.x + Vector2.up * max.y);
+        Gizmos.DrawLine(origin + Vector2.right * min.x + Vector2.up * max.y, origin + Vector2.right * min.x + Vector2.up * min.y);
+        Gizmos.DrawLine(origin + Vector2.right * min.x + Vector2.up * min.y, origin + Vector2.right * max.x + Vector2.up * min.y);
+        Gizmos.DrawLine(origin + Vector2.right * max.x + Vector2.up * min.y, origin + Vector2.right * max.x + Vector2.up * max.y);
     }
 #endif
 }
