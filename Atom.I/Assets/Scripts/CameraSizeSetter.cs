381f268da93ce6b0c80ec9e94e561ca410cd5fd4
xdgorgola <15-11264@usb.ve>
2020-03-26 15:26:44 -0430

    Camera Sizer

---
+++ Atom.I/Assets/Scripts/CameraSizeSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraSizeSetter : MonoBehaviour
+{
+    public float x = 10;
+    public float y = 10;
+
+    void Update()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height; // Que tanto X hay por cada Y actualmente
+        float targetRatio = x / y; // Que tanto X quiero que haya por Y
+
+        if (screenRatio >= targetRatio) // Si hay mas de lo que quiero, me asegura que alcance si seteo la Y
+        {
+            Camera.main.orthographicSize = y / 2;
+        }
+        else // Si no, calculamos la diferencia
+        {
+            float differenceInSize = targetRatio / screenRatio; // Que tanto de lo que quiero es lo que tengo
+            Camera.main.orthographicSize = (y / 2) * differenceInSize;
+        }
+    }
+}
