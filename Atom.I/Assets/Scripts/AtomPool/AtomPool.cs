91f084130c4db1ac8125d752a4d7cc86a68549a0
xdgorgola <15-11264@usb.ve>
2020-03-26 01:48:03 -0430

    Atom pool

---
+++ Atom.I/Assets/Scripts/AtomPool/AtomPool.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomPool : MonoBehaviour
+{
+    /// <summary>
+    /// Pool
+    /// </summary>
+    public static AtomPool Pool { get; private set; }
+    /// <summary>
+    /// Devuelve un atomo
+    /// </summary>
+    public static GameObject FreeAtom
+    {
+        get { return Pool.GetAtom(); }
+    }
+
+    /// <summary>
+    /// Prefab atomo
+    /// </summary>
+    public GameObject atomPrefab;
+    public int initialAtoms = 30;
+    private List<GameObject> atoms;
+
+    private void Awake()
+    {
+        if (Pool != null && Pool != this)
+        {
+            Debug.LogWarning("La pool de atomos ya existe! Eliminando este objeto...", gameObject);
+            Destroy(gameObject);
+        }
+        Pool = this;
+
+        atoms = new List<GameObject>(initialAtoms);
+        for (int i = 0; i < initialAtoms; i++)
+        {
+            GameObject atom = Instantiate(atomPrefab);
+            atom.SetActive(false);
+            atoms.Add(atom);
+        }
+    }
+
+    /// <summary>
+    /// Obtiene un GameObject del prefab Atomo
+    /// </summary>
+    /// <returns>GameObject prefab Atomo</returns>
+    private GameObject GetAtom()
+    {
+        foreach (GameObject atom in atoms)
+        {
+            if (!atom.activeInHierarchy)
+            {
+                return atom;
+            }
+        }
+        GameObject newAtom = Instantiate(atomPrefab);
+        atoms.Add(newAtom);
+        newAtom.SetActive(false);
+        return newAtom;
+    }
+}
---
+++ Atom.I/Assets/Scripts/GameManager/GameManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum GameState { Starting, Playing, Paused, GameOver }
+public class GameManagerScript : MonoBehaviour
+{
+    public GameState state { get; private set; } = GameState.Starting;
+
+    [HideInInspector]
+    public int level { get; private set; } = 0;
+    [SerializeField]
+    [Range(1, 10)]
+    private int maxLevel = 7;
+
+    private float remainingTime = 0;
+    public List<float> levelTime = new List<float>();
+
+    private void Start()
+    {
+        level = 0;
+        remainingTime = levelTime.Sum();
+    }
+
+    private IEnumerator LevelTime()
+    {
+        yield return new WaitForSeconds(0);
+    }
+}
