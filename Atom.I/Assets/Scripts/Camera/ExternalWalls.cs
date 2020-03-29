fcae72aeaca8bf5ee3e79cfccb1e71ee81669332
xdgorgola <15-11264@usb.ve>
2020-03-29 17:49:12 -0430

    Paredes externas

---
+++ Atom.I/Assets/Scripts/Camera/ExternalWalls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalWalls : MonoBehaviour
+{
+    public GameObject topWall = null;
+    public GameObject bottomWall = null;
+    public GameObject leftWall = null;
+    public GameObject rightWall = null;
+
+    private float h;
+    private float w;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        h = Camera.main.orthographicSize * 2;
+        w = ((float)Screen.width / (float)Screen.height) * h;
+
+        topWall.transform.position = Vector2.up * (h / 2 + 0.5f);
+        bottomWall.transform.position = Vector2.up * (-h / 2 - 0.5f);
+        rightWall.transform.position = Vector2.right * (w / 2 + 0.5f);
+        leftWall.transform.position = Vector2.right * (-w / 2 - 0.5f);
+    }
+}
