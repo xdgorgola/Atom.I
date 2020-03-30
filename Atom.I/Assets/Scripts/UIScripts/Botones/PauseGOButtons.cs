b681616d5651fa960390fd2a6385ae00f9188222
xdgorgola <15-11264@usb.ve>
2020-03-29 20:41:26 -0430

    Particulas/Script botones

---
+++ Atom.I/Assets/Scripts/UIScripts/Botones/PauseGOButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGOButtons : MonoBehaviour
+{
+    public GameObject pauseGOButtons = null;
+    public GameObject nextButtons = null;
+
+    private void Start()
+    {
+        if (pauseGOButtons == null || nextButtons == null)
+        {
+            Debug.LogWarning("Aparecedor de botones no seteado");
+            return;
+        }
+        if (GameManagerScript.Manager != null)
+        {
+            GameManagerScript.Manager.onGameOver.AddListener(() => pauseGOButtons.SetActive(true));
+            GameManagerScript.Manager.onPause.AddListener(() => pauseGOButtons.SetActive(true));
+            GameManagerScript.Manager.onResume.AddListener(() => pauseGOButtons.SetActive(false));
+
+            GameManagerScript.Manager.onFinishedGame.AddListener(() => nextButtons.SetActive(true));
+        }
+
+        pauseGOButtons.SetActive(false);
+        nextButtons.SetActive(false);
+    }
+}
