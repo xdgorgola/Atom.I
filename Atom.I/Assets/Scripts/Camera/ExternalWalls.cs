80c3e8f4c77e4c8a048c22f41d9079b1be572919
xdgorgola <15-11264@usb.ve>
2020-03-31 18:46:30 -0430

    UI Barra

--- Atom.I/Assets/Scripts/Camera/ExternalWalls.cs
+++ Atom.I/Assets/Scripts/Camera/ExternalWalls.cs
@@ -21,8 +21,12 @@ public class ExternalWalls : MonoBehaviour
         topWall.transform.position = Vector2.up * (h / 2 + 0.5f);
         topWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * w;
 
-        bottomWall.transform.position = Vector2.up * (-h / 2 - 0.5f);
-        bottomWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * w;
+        float quarter = Screen.height * 0.13f;
+        float size = Mathf.Abs((Camera.main.ScreenToWorldPoint(Vector3.up * quarter) - Camera.main.ScreenToWorldPoint(Vector3.zero)).magnitude);
+        Debug.Log(size);
+        //bottomWall.transform.position = Vector2.up * (-h / 2 - 0.5f);
+        bottomWall.transform.position = Camera.main.ScreenToWorldPoint(Vector3.up * (quarter / 2) + Vector3.right * (Screen.width / 2));
+        bottomWall.transform.localScale = Vector3.up * size + Vector3.forward + Vector3.right * w;
 
         rightWall.transform.position = Vector2.right * (w / 2 + 0.5f);
         rightWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * h;
