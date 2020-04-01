3f1982dac434f5f10d4b49338f6eaffbba323fec
xdgorgola <15-11264@usb.ve>
2020-03-31 22:46:35 -0430

    Ya no se sale todo

--- Atom.I/Assets/Scripts/Camera/ExternalWalls.cs
+++ Atom.I/Assets/Scripts/Camera/ExternalWalls.cs
@@ -18,8 +18,13 @@ public class ExternalWalls : MonoBehaviour
         h = Camera.main.orthographicSize * 2;
         w = ((float)Screen.width / (float)Screen.height) * h;
 
-        topWall.transform.position = Vector2.up * (h / 2 + 0.5f);
-        topWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * w;
+        float x = GetComponent<CameraSizeSetter>().x;
+        float y = GetComponent<CameraSizeSetter>().y;
+
+        //topWall.transform.position = Vector2.up * (h / 2 + 0.5f);
+        topWall.transform.position = Vector2.up * y / 2;
+        //topWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * w;
+        topWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * x;
 
         float quarter = Screen.height * 0.13f;
         float size = Mathf.Abs((Camera.main.ScreenToWorldPoint(Vector3.up * quarter) - Camera.main.ScreenToWorldPoint(Vector3.zero)).magnitude);
@@ -28,10 +33,13 @@ public class ExternalWalls : MonoBehaviour
         bottomWall.transform.position = Camera.main.ScreenToWorldPoint(Vector3.up * (quarter / 2) + Vector3.right * (Screen.width / 2));
         bottomWall.transform.localScale = Vector3.up * size + Vector3.forward + Vector3.right * w;
 
-        rightWall.transform.position = Vector2.right * (w / 2 + 0.5f);
-        rightWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * h;
 
-        leftWall.transform.position = Vector2.right * (-w / 2 - 0.5f);
+        //rightWall.transform.position = Vector2.right * (w / 2 + 0.5f);
+        rightWall.transform.position = Vector2.right * (x / 2);
+        rightWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * y;
+
+        //leftWall.transform.position = Vector2.right * (-w / 2 - 0.5f);
+        leftWall.transform.position = Vector2.right * (-x / 2);
         leftWall.transform.localScale = Vector3.up + Vector3.forward + Vector3.right * h;
     }
 }
